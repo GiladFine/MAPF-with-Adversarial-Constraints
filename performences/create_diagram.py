876b9d9b82f101c746445b1a7c6d35058d2f9614
Gilad Fine <giladfine1@gmail.com>
2022-10-28 13:13:06 +0300

    results

--- performences/create_diagram.py
+++ performences/create_diagram.py
@@ -4,6 +4,7 @@ import numpy as np
 import json
 
 NUMBER_OF_RUNS = 100
+MAP_NAME = "room-32.map"
 
 def read_results():
     with open("performence_results.json") as res_file:
@@ -17,7 +18,7 @@ def plot_sat_graph(mode):
     labels = set()
     variations = []
     arrays_dict = defaultdict(list)
-    for variation, data in result["room-32.map"].items():
+    for variation, data in result[MAP_NAME].items():
         variations.append(variation)
         for num_of_agents, data_of_agents_amount in data.items():
             labels.add(int(num_of_agents))
@@ -50,6 +51,16 @@ def plot_sat_graph(mode):
                     / NUMBER_OF_RUNS,
                     1
                 )
+            elif mode == "avg_paths_length":
+                value = round(
+                    (
+                        data_of_agents_amount["overall_paths_sizes_generally_sat"]
+                        / (
+                            int(num_of_agents) * result[MAP_NAME]["stays"][num_of_agents]["number_of_sat_solutions"]
+                        )
+                    ),
+                    1
+                )
             else:
                 raise ValueError("BAD MODE")
             arrays_dict[variation].append(value)
@@ -59,9 +70,9 @@ def plot_sat_graph(mode):
     width = 0.3  # the width of the bars
 
     fig, ax = plt.subplots()
-    rects1 = ax.bar(x - width, arrays_dict[variations[0]], width, label=variations[0], hatch='///')
-    rects2 = ax.bar(x, arrays_dict[variations[1]], width, label=variations[1], hatch='.O')
-    rects3 = ax.bar(x + width, arrays_dict[variations[2]], width, label=variations[2], hatch='..')
+    rects1 = ax.bar(x - width, arrays_dict[variations[0]], width, label='SOT', hatch='///')
+    rects2 = ax.bar(x, arrays_dict[variations[1]], width, label='DOT', hatch='.O')
+    rects3 = ax.bar(x + width, arrays_dict[variations[2]], width, label='HOT', hatch='..')
 
 
     # Add some text for labels, title and custom x-axis tick labels, etc.
@@ -70,14 +81,21 @@ def plot_sat_graph(mode):
         ax.set_ylabel('% of SAT instances')
         plt.ylim(0, 130)
     elif mode == "SAT-times":
-        ax.set_ylabel('Avarage time per SAT instance (seconds)')
-        plt.ylim(0, 70)
+        ax.set_ylabel('Average time per SAT instance (seconds)')
+        ylim = 40 if MAP_NAME == "room-32.map" else 120
+        plt.ylim(0, ylim)
     elif mode == "non-SAT-times":
-        ax.set_ylabel('Avarage time per non-SAT instance (seconds)')
-        plt.ylim(0, 70)
+        ax.set_ylabel('Average time per non-SAT instance (seconds)')
+        ylim = 40 if MAP_NAME == "room-32.map" else 120
+        plt.ylim(0, ylim)
     elif mode == "overall-times":
-        ax.set_ylabel('Avarage time per instance (seconds)')
-        plt.ylim(0, 70)
+        ax.set_ylabel('Average time per instance (seconds)')
+        ylim = 40 if MAP_NAME == "room-32.map" else 120
+        plt.ylim(0, ylim)
+    elif mode == "avg_paths_length":
+        ax.set_ylabel('Average path length for SAT instances')
+        ylim = 40 if MAP_NAME == "room-32.map" else 90
+        plt.ylim(0, ylim)
     else:
         raise ValueError("BAD MODE")
     
@@ -92,7 +110,7 @@ def plot_sat_graph(mode):
 
     fig.tight_layout()
     plt.show()
-    # plt.savefig(mode + ".png")
+    # plt.savefig("room-32-graphs/" + mode + ".png")
            
 
 
@@ -101,3 +119,4 @@ if __name__ == "__main__":
     plot_sat_graph("SAT-times")
     plot_sat_graph("non-SAT-times")
     plot_sat_graph("overall-times")
+    plot_sat_graph("avg_paths_length")
